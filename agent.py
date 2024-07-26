cd6d51ff1bd9bfb0995585d2d9f77c084d6782a6
Ivan Popravka <ivan.edu.popravka@yandex.ru>
2024-07-26 20:23:51 +0300

    Update game rules

--- agent.py
+++ agent.py
@@ -1,6 +1,5 @@
-# Baseline Solution
-
 import csv
+import time
 import requests
 
 class User:
@@ -18,7 +17,7 @@ class YandexGPTApi:
             "Authorization": f"Api-Key {api_key}"
         }
 
-    def send_prompt(self, system_text, user_text, temperature=0.6, max_tokens=10):
+    def send_prompt(self, system_text, user_text, temperature=1, max_tokens=50):
         prompt = {
             "modelUri": self.model_uri,
             "completionOptions": {
@@ -48,27 +47,32 @@ class CentipedeGame:
         self.user2 = user2
         self.max_rounds = max_rounds
         self.current_round = 1
-        self.pot = 1
+        self.pot_big = 4
+        self.pot_small = 1
         self.history = []
         self.is_game_over = False
 
     def get_prompt_for_user(self, user, opponent, history):
-        system_text = f"Вы участвуете в игре. Правила игры: {self.get_rules()}."
+        system_text = (
+            f"Вы учавствуете в игре. Ваш оппонент {opponent}. Правила игры: {self.get_rules()}."
+            "То,  сколько вы заработаете зависит от ваших решений и решений оппонента.")
         user_text = (
-            f"Ваше текущее эмоциональная состояние: {user.emotional_state}. "
+            f"Предположим, чтоы вы сейчас {user.emotional_state}. Ваше состояние может повлиять на ваш выбор."
+            f"Текущее эмоциональное состояние вашего оппонента: {opponent.emotional_state}"
             f"История ходов: {history}. "
-            'Дайте ответ одним словом, хотите ли вы "Взять" или "Пасс"?' # указание, что ответ должен быть одним словом существенно!
-        )
+            "Сейчас вы можете взять себе {self.pot_big} монет и отдать оппоненту {self.pot_small} монет}"
+            'Дайте ответ одним и только одним словом: "Взять" или "Пас"?')
         return system_text, user_text
 
-    def get_rules(self): # неверные правила, потом перепишем
+    def get_rules(self): # Дописать нормально правила
         return (
-            "Игра включает двух игроков, которые по очереди могут взять текущий банк денег или передать его другому игроку, удвоив его сумму. "
-            f"Игра заканчивается, когда один игрок берет банк, или после фиксированного числа раундов. Всего {self.max_rounds} раундов. Сейчас {self.current_round} раунд. "
-            "В каждом раунде игрок должен выбрать одну из двух опций: 'Пасс' (передать банк оппоненту, удвоив сумму) или 'Взять' (забрать текущий банк). "
-            "Если игрок 1 берет банк на первом ходу: игрок 1 получает 2, игрок 2 получает 1. "
-            "Если игрок 1 передает, и игрок 2 берет банк: игрок 2 получает 4, игрок 1 получает 3. "
-            "Если оба игрока пасуют на последнем ходу, последний пасующий получает сумму банка минус 1, а другой игрок получает сумму банка плюс 1."
+            "Игра включает двух игроков. В начале игры перед первым игроком находятся две стопки монет."
+            f"Одна стопка содержит 4 монеты, а другая 1 монету. У каждого игрока есть два возможных хода:"
+            "либо 'взять' большую стопку монет и отдать меньшую стопку другому игроку, либо 'пасануть' обе стопки через стол другому игроку."
+            "Каждый раз, когда вы пасуете, стопки монет передаются через стол, количество монет в каждой стопке удваивается."
+            "Игра продолжается до тех пор, пока один из игроков не решит завершить игру выбрав действие 'взять' и забрав себе большую стопку монет или по истечению фиксированного числа раундов."
+            f"Всего {self.max_rounds} раундов. Сейчас {self.current_round} раунд."
+            "Если игрок на последнем раунде 'пасует', то оба игрока делят большую стопку пополам, но тот кто 'пасанул' получиит на одну монету меньше" # TO-DO: нормально сформулировать
         )
 
     def play_round(self, api, model_uri):
@@ -85,52 +89,45 @@ class CentipedeGame:
 
         if move == 'взять':
             self.end_game(user)
-        else: # нормально прописать условие, чтобы быть уверенным, что там нет ошибки
-            self.history.append(f"Раунд {self.current_round}: {user.name} пасс")
-            self.pot *= 2
+        elif move == 'пас':
+            self.history.append(f"Раунд {self.current_round}: {user.name} пас")
+            self.pot_big *= 2
+            self.pot_small *= 2 # Вроде бы тут где-то ошибка
             self.current_round += 1
             if self.current_round > self.max_rounds:
                 self.end_game()
+        else:
+            raise ValueError
 
     def end_game(self, taker=None):
         self.is_game_over = True
         if taker:
             if taker == self.user1:
-                self.history.append(f"{self.user1.name} забрал(а) банк! {self.user1.name} получает {self.pot * 2}, {self.user2.name} получает {self.pot - 1}")
+                self.history.append(f"{self.user1.name} забрал(а) банк! {self.user1.name} получает {self.pot_big * 2}, {self.user2.name} получает {self.pot_small}")
             else:
-                self.history.append(f"{self.user2.name} забрал(а) банк! {self.user2.name} получает {self.pot * 2}, {self.user1.name} получает {self.pot - 1}")
+                self.history.append(f"{self.user2.name} забрал(а) банк! {self.user2.name} получает {self.pot_big * 2}, {self.user1.name} получает {self.pot_small}")
         else:
             last_passer = self.user1 if self.current_round % 2 != 0 else self.user2
             other_user = self.user2 if last_passer == self.user1 else self.user1
-            self.history.append(f"Оба пассанул. {last_passer.name} получает {self.pot - 1}, {other_user.name} получает {self.pot + 1}")
+            self.history.append(f"Оба пасанули. {last_passer.name} получает {self.pot_big - 1}, {other_user.name} получает {self.pot_big + 1}")
 
+        print(self.history)
 
+    def log_results():
+        # TO-DO: сохранить результаты каждой игры в json/csv файл.
 
 
 
 api_key = "YOUR-API"
-model_uri = "gpt://YOUR-DIR/yandexgpt-lite"
-system_text = "Ты ассистент, способный решить задачи по оптимизации"
-user_text = "Привет, ассистент! Мне нужна твоя помощь. Как найти градиент функции по тензору?"
+model_uri = "gpt://YOUR-DIR/yandexgpt/latest" # Поставить разные версии YandexGPT
 
 api = YandexGPTApi(api_key, model_uri)
 
-#response = api.send_prompt(system_text, user_text)
-
-#print(response) # Работает!
-
-#Для того, чтобы найти **градиент функции по тензору**, вам следует использовать **метод конечных элементов** или **конечно-разностный метод**.
-
-#Для использования метода конечных элементов вам необходимо:
-
-##1. Разбить область определения тензора на элементы (треугольники, квадраты и т.д.).
-#2. Заменить тензор в каждом элементе на скалярную величину, которая называется **интерполяционной функцией**.
-
-
 # Потом перепишем
-user1 = User('Алиса', emotional_state='злая')
-user2 = User('Ваня', emotional_state='спокойный')
+user1 = User('Пользователь_1', emotional_state='добрый')
+user2 = User('Пользователь_2', emotional_state='злой')
 
-game = CentipedeGame(user1=user1, user2=user2, max_rounds=3)
+game = CentipedeGame(user1=user1, user2=user2, max_rounds=5)
 while game.current_round <= game.max_rounds and not game.is_game_over:
+    time.sleep(2) # Иначе TimeOutError
     game.play_round(api, model_uri)
