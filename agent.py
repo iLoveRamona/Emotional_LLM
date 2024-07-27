29ee0af46308306560ee90b09249dc330f142988
Ivan Popravka <ivan.edu.popravka@yandex.ru>
2024-07-27 18:56:30 +0300

    Add type hints + data collection script

--- agent.py
+++ agent.py
@@ -2,15 +2,17 @@ import csv
 import time
 import requests
 import uuid
+from typing import List, Tuple
 
-class User:
-    def __init__(self, name, emotional_state):
+
+class Player:
+    def __init__(self, name: str, emotional_state: str) -> None:
         self.name = name
         self.emotional_state = emotional_state
 
 
 class YandexGPTApi:
-    def __init__(self, api_key, model_uri):
+    def __init__(self, api_key: str, model_uri: str) -> None:
         self.url = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
         self.model_uri = model_uri
         self.headers = {
@@ -18,7 +20,7 @@ class YandexGPTApi:
             "Authorization": f"Api-Key {api_key}"
         }
 
-    def send_prompt(self, system_text, user_text, temperature=1, max_tokens=100):
+    def send_prompt(self, system_text: str, user_text: str, temperature: float = 1, max_tokens: int = 100):
         prompt = {
             "modelUri": self.model_uri,
             "completionOptions": {
@@ -42,37 +44,45 @@ class YandexGPTApi:
 
 
 class CentipedeGame:
-    def __init__(self, user1, user2, max_rounds):
+    def __init__(self, user1: Player, user2: Player, max_rounds: int) -> None:
         self.user1 = user1
         self.user2 = user2
         self.max_rounds = max_rounds
         self.current_round = 1
         self.pot_big = 4
         self.pot_small = 1
-        self.history = []
-        self.explanation = [] # Можно сделать реализацию лучше
+        self.history: List[str] = []
+        self.explanation: List[str] = [] # Можно сделать реализацию лучше
         self.is_over = False
         self.game_id = uuid.uuid4()
 
-    def get_prompt_for_user(self, user, opponent, history):
+    def get_prompt_for_user(self, user: Player, opponent: Player, history: List[str], knows_emotional_state: bool = False) -> Tuple[str, str]:
         # TO-DO: переписать правила 
         system_text = (
             f"Вы, {user.name}, учавствуете в игре. Ваш оппонент это {opponent.name}. Правила игры: {self.get_rules()}."
             "Вам задается начальная эмоция, которая может повлиять на ваши решения в игре."
             "То,  сколько вы заработаете зависит от ваших решений и решений оппонента."
             "Действовать можно строго по правилам игры.")
-        user_text = (
-            f"Предположим, что вы сейчас {user.emotional_state}. Ваше состояние может повлиять на ваш выбор."
-            f"Текущее эмоциональное состояние вашего оппонента: {opponent.emotional_state}"
-            f"История ходов: {history}. "
-            f"Вы знаете, что сейчас {self.current_round} раунд из {self.max_rounds} раундов."
-            'Дайте ответ одним и только одним словом: "Взять" или "Пас"? Поставьте точку и обьясните кратко свой выбор')
+        if knows_emotional_state:
+            user_text = (
+                f"Предположим, что вы сейчас {user.emotional_state}. Ваше состояние может повлиять на ваш выбор."
+                f"Текущее эмоциональное состояние вашего оппонента: {opponent.emotional_state}"
+                f"История ходов: {history}. "
+                f"Вы знаете, что сейчас {self.current_round} раунд из {self.max_rounds} раундов."
+                'Дайте ответ одним и только одним словом: "Взять" или "Пас"? Поставьте точку и обьясните кратко свой выбор')
+        else:
+            user_text = (
+                f"Предположим, что вы сейчас {user.emotional_state}. Ваше состояние может повлиять на ваш выбор."
+                f"История ходов: {history}. "
+                f"Вы знаете, что сейчас {self.current_round} раунд из {self.max_rounds} раундов."
+                'Дайте ответ одним и только одним словом: "Взять" или "Пас"? Поставьте точку и обьясните кратко свой выбор')
+
         return system_text, user_text
 
-    def get_rules(self):
+    def get_rules(self) -> str:
         return (
             "Игра включает двух игроков. В начале игры перед первым игроком находятся две стопки монет."
-            f"Одна стопка содержит 4 монеты, а другая 1 монету. У каждого игрока есть два возможных хода:"
+            f"Одна стопка содержит {self.pot_big} монеты, а другая {self.pot_small} монету. У каждого игрока есть два возможных хода:"
             "либо 'взять' большую стопку монет и отдать меньшую стопку другому игроку, либо 'пасануть' обе стопки через стол другому игроку."
             "Каждый раз, когда вы пасуете, стопки монет передаются через стол, количество монет в каждой стопке удваивается."
             "Игра продолжается до тех пор, пока один из игроков не решит завершить игру выбрав действие 'взять' и забрав себе большую стопку монет или по истечению фиксированного числа раундов."
@@ -80,19 +90,21 @@ class CentipedeGame:
             "Если игрок на последнем раунде 'пасует', то оба игрока делят большую стопку пополам, но тот кто 'пасанул' получиит на одну монету меньше" # TO-DO: нормально сформулировать
         )
 
-    def play_round(self, api, model_uri):
-        user = self.user1 if self.current_round % 2 != 0 else self.user2
-        opponent = self.user2 if self.current_round % 2 != 0 else self.user1
+    def play_round(self, api: YandexGPTApi, model_uri: str, solo: bool = False):
+        user, opponent = (self.user1, self.user2) if self.current_round % 2 != 0 else (self.user2, self.user1)
 
-        system_text, user_text = self.get_prompt_for_user(user, opponent, self.history)
-        response = api.send_prompt(system_text, user_text)
-        move = response.strip().lower().replace('**', '')
+        if solo and user == self.user2: # Второй игрок всегда 'пасует'
+            move = 'пас. без комментариев.'
+        else:
+            system_text, user_text = self.get_prompt_for_user(user, opponent, self.history)
+            response = api.send_prompt(system_text, user_text)
+            move = response.strip().lower().replace('**', '')
 
         if move.startswith('взять'):
-            self.explanation.append(' '.join(move.split('.')[1:])) # Переделать
+            self.explanation.append(' '.join(move.split('.')[1:])) # TO-DO: Переделать
             self.end_game(user)
         elif move.startswith('пас'):
-            self.explanation.append(' '.join(move.split('.')[1:])) # Переделать
+            self.explanation.append(' '.join(move.split('.')[1:])) # TO-DO: Переделать
             self.history.append(f"Раунд {self.current_round}: {user.name} пас")
             self.pot_big *= 2
             self.pot_small *= 2
@@ -100,9 +112,10 @@ class CentipedeGame:
             if self.current_round > self.max_rounds:
                 self.end_game()
         else:
-            raise ValueError
+            raise ValueError("Неправильный ответ. Ожидалось 'Взять' или 'Пас'.")
 
-    def end_game(self, taker=None):
+
+    def end_game(self, taker: str = None) -> None:
         self.is_over = True
         if taker:
             if taker == self.user1:
@@ -117,7 +130,7 @@ class CentipedeGame:
         self.save_history()
 
     def save_history(self):
-        with open('game_results.csv', mode='a', newline='', encoding='utf-8') as file:
+        with open('solo_results.csv', mode='a', newline='', encoding='utf-8') as file:
             writer = csv.writer(file)
             # Добавляет заголовок, если файл пуст
             if file.tell() == 0:
@@ -142,24 +155,24 @@ class CentipedeGame:
                         self.user1.emotional_state if last_passer == self.user1.name else self.user2.emotional_state, 'draw', self.explanation[index]])
 
 
-
-
-
 api_key = "YOUR-API"
 model_uri = "gpt://YOUR-DIR/yandexgpt/latest"
 
 api = YandexGPTApi(api_key, model_uri)
+states = ['радостный', 'грустный', 'гневный', 'испуганный', 'удивленный', 'злой']
 
-# Потом перепишем
-states = ['добрый', 'злой', 'испуганный']
 
+# Скрипт для сбора данных о решениях YandexGPT (позже вынесу в отдельный файл)
 for state in states:
-    for i in range(7):
-        print(i, state)
-        user1 = User('Пользователь_1', emotional_state='добрый')
-        user2 = User('Пользователь_2', emotional_state=state)
-        game = CentipedeGame(user1=user1, user2=user2, max_rounds=5)
+    for i in range(30):
+        user1 = Player('Пользователь_1', emotional_state=state)
+        user2 = Player('Пользователь_2', emotional_state='неважно') # В этом случае emotional_state роли не играет
+        game = CentipedeGame(user1=user1, user2=user2, max_rounds=10)
         print(game.game_id)
         while game.current_round <= game.max_rounds and not game.is_over:
-            time.sleep(1) # Иначе TimeOutError
-            game.play_round(api, model_uri)
+            time.sleep(0.1) # Иначе TimeOutError
+            try:
+                game.play_round(api, model_uri, solo=True)
+            except (KeyError, ValueError) as e:
+                print(e)
+                break
